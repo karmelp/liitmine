4a98726902ddfb68df71580ddfc4e45683fbe95f
karmelp <101389533+karmelp@users.noreply.github.com>
2022-09-22 13:45:10 +0300

    getInputValues()

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ function getSum(arr1, arr2) {
     return sum
 }
 
+function getInputValues() {
+    const liidetav1 = parseInt(document.querySelector('#liidetav1').value);
+    const liidetav2 = parseInt(document.querySelector('#liidetav2').value);
+
+    return {liidetav1, liidetav2}
+}
+
 function getFeedback() {
     if(document.querySelector('input').value.length == 0){
         document.querySelector('.summa').innerHTML = 'Ma ei saa öelda, mis summa on, kui sa ei ütle, mida liita!'
     } else {
-        const liidetav1 = parseInt(document.querySelector('#liidetav1').value);
-        const liidetav2 = parseInt(document.querySelector('#liidetav2').value);
+        const {liidetav1, liidetav2} = getInputValues()
         getSum(liidetav1, liidetav2)
         document.querySelector('.summa').innerHTML = `Kui liita ${liidetav1} ja ${liidetav2}, saame ${getSum(liidetav1, liidetav2)}.`
     }
